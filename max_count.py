d10998ff4739f2fb6e00611b665aa8a083b31d43
Shubham Pawar <shubham@Shubhams-MacBook-Air.local>
2023-01-17 19:13:04 -0600

    MaxCount

--- max_count.py
+++ max_count.py
@@ -1,3 +1,19 @@
+"""
+    Maximum Count of Positive Integer and Negative Integer
+
+    User Accepted: 11422
+    User Tried: 11755
+    Total Accepted: 11782
+    Total Submissions: 14750
+    Difficulty: Easy
+
+Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+
+    In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+
+Note that 0 is neither positive nor negative.
+"""
+
 pos = 0
 neg = 0
 class Solution:
